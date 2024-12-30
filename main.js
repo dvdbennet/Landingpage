c0c5566b13668e3d363e56d07ef06ad825497042
dvdbennet <168564922+dvdbennet@users.noreply.github.com>
2024-12-30 19:00:12 -0300

    arrumando as perguntas

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('.faq-question');
+  
+    questions.forEach(question => {
+      question.addEventListener('click', function() {
+        const answer = this.nextElementSibling;
+  
+        // Fechar todas as respostas abertas, exceto a atual
+        document.querySelectorAll('.faq-answer').forEach(ans => {
+          if (ans !== answer) {
+            ans.style.display = 'none';
+          }
+        });
+  
+        // Alternar a exibição da resposta atual
+        if (answer.style.display === 'block') {
+          answer.style.display = 'none';
+        } else {
+          answer.style.display = 'block';
+        }
+      });
+    });
+  });
